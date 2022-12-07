5998e20bda7567b70debd7e63aebd701fd2f6ef6
Mark Ghattas <mag186@duke.edu>
2022-12-07 08:53:50 -0500

    class 12/6

---
+++ Assets/LightScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+   /* public Light myLight;
+
+    //range variables
+    public bool changeRange=false;
+    public float rangeSpeed = 1.0f;
+    public float maxRange = 1.0f;
+    public bool change =true;
+    public float timesampler = 0f;
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*timesampler=timesampler+deltatime;
+        for(int i=0;i<4;i++){
+            //if change==true, increase intensity, false- decrease intensity
+            //
+            if(timesampler>4){
+                timesampler=0;
+                if(change==true){
+                change=false;
+                }
+                if(change==false){
+                change==true;
+                }
+            }
+        }*/
+    }
+}
