2bc832bc873201a6e5423827be2c85baf137045d
Mark Ghattas <mag186@duke.edu>
2022-12-07 09:29:59 -0500

    last crit

--- Assets/LightScript.cs
+++ Assets/LightScript.cs
@@ -4,37 +4,44 @@ using UnityEngine;
 
 public class LightScript : MonoBehaviour
 {
-   /* public Light myLight;
+    public Light myLight;
 
     //range variables
-    public bool changeRange=false;
+    float deltatime=Time.deltaTime;
     public float rangeSpeed = 1.0f;
     public float maxRange = 1.0f;
     public bool change =true;
     public float timesampler = 0f;
-    */
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+         //myLight.intensity=Mathf.PingPong(Time.time,8);
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*timesampler=timesampler+deltatime;
+         /*if(change==true){
+                myLight.intensity=Mathf.PingPong(Time.time,4);
+            }*/
+       timesampler=timesampler+deltatime;
         for(int i=0;i<4;i++){
-            //if change==true, increase intensity, false- decrease intensity
-            //
+            if(change==true){
+                myLight.intensity=Mathf.PingPong(Time.time,8);
+            }
+            if(change==false){
+                myLight.intensity=Mathf.PingPong(Time.time,8);
+            }
             if(timesampler>4){
                 timesampler=0;
                 if(change==true){
                 change=false;
                 }
                 if(change==false){
-                change==true;
+                change=true;
                 }
             }
-        }*/
+        }
     }
 }
